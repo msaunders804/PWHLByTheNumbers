0614c96a2c5f3d4358d66cc809eb2e7c80745efb
msaunders804 <msaunders804@yahoo.com>
2026-03-05 19:15:52 -0600

    CLeaning up Repo

--- DEPRECATED/test.py
+++
@@ -1,8 +0,0 @@
-from db_config import get_db_url
-url = get_db_url()
-print('Connecting to:', url[:50], '...')
-from sqlalchemy import create_engine, text
-engine = create_engine(url)
-with engine.connect() as conn:
-    row = conn.execute(text('SELECT MAX(game_id) FROM games')).fetchone()
-    print('Max game_id in DB:', row[0])
